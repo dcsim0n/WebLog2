7ae9e7d9b8d6e7e9b49d671cabbfb7330549c8f5
Dana Simmons <dsanalog@gmail.com>
2019-11-17 14:52:56 -0500

    Hook sequelize into express
    imported sequelize object into express www script.
    
    Call sequelize.sync() before starting the app.
    
    models are imported from models/index.js as db.ModelName

--- models/fieldtype.js
+++ models/fieldtype.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   FieldType.associate = function(models) {
     // associations can be defined here
-    FieldType.hasMany( models.Fields );
+    FieldType.hasMany( models.Field );
   };
   return FieldType;
 
